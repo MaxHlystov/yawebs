26d37a06afa59b4bfa7afc33bef660a75beb8420
Maxim <maxvls@gmail.com>
2016-02-25 23:05:43 +0300

    Update startdemo.sh

--- startdemo.sh
+++ startdemo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 webdir=`pwd`
-$webdir/final --debug 20 -h 127.0.0.1 -p 12345 -d "$webdir" > /dev/null
+$webdir/final -h 127.0.0.1 -p 12345 -d "$webdir" > /dev/null
 echo "Wait while server has started..."
 sleep 1
 echo "To test in command line use: curl -0 -X GET 127.0.0.1:12345"
