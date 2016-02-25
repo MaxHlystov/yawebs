3e7cdcf86745f56b551ee91f42205b1a559813f8
Maxim <maxvls@gmail.com>
2016-02-25 23:28:33 +0300

    Update loadtest.sh

--- loadtest.sh
+++ loadtest.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 echo "Start load test. We will connect to web-server 1000 times."
-maxi=100
+maxi=1000
 i=0
 while [[ $i -lt $maxi ]]
 do
 	echo "Test: $i"
-	curl -0 -X GET 127.0.0.1:12345?i=$i > /dev/null
+	curl -s -0 -X GET 127.0.0.1:12345?i=$i > /dev/null
 	let	"i += 1"
+	
+	let "p = i % 50"
+	if [[ $p -eq 0 ]]
+	then
+		echo "Test #$i"
+	fi
 done
 
 echo "End load test"
